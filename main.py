ae1dc46a13a4f705206a42c43359c301e02c589d
siddartha sagar chinne <siddarthasagar@gmail.com>
2025-01-10 19:56:00 +0530

    basics of python

---
+++ main.py
@@ -0,0 +1,13 @@
+import calendar
+import datetime
+"""Will this work?"""
+print("hello", "Sowmya", sep=",", end="!\n" )
+print(7)
+print("""single and double quotes are interchangable  
+this is a \"multi line\" text""")
+#comment line
+
+""" Modules are two types : Internal and External
+Internal are inbuilt
+External : add dependencies using pip install
+"""
